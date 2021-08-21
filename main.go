4e923ebfa4581f557993ee833fbe265e98df7f34
inject <66717990+flag007@users.noreply.github.com>
2021-08-22 00:38:48 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	var appendMode bool
+	flag.BoolVar(&appendMode, "a", false, "Append the value instead of replacing it")
+	flag.Parse()
+
+	seen := make(map[string]bool)
+
+	// read URLs on stdin, then replace the values in the query string
+	// with some user-provided value
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		u, err := url.Parse(sc.Text())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to parse url %s [%s]\n", sc.Text(), err)
+			continue
+		}
+
+		// Go's maps aren't ordered, but we want to use all the param names
+		// as part of the key to output only unique requests. To do that, put
+		// them into a slice and then sort it.
+		pp := make([]string, 0)
+		vv := make([]string, 0)
+		for p, v := range u.Query() {
+			pp = append(pp, p)
+			vv = append(vv,v[0])
+		}
+		sort.Strings(pp)
+		sort.Strings(vv)
+
+		fmt.Println(pp)
+		fmt.Println(vv)
+		key := fmt.Sprintf("%s%s?%s", u.Hostname(), u.EscapedPath(), strings.Join(pp, "&"))
+
+		fmt.Println(key)
+		// Only output each host + path + params combination once
+		if _, exists := seen[key]; exists {
+			continue
+		}
+		seen[key] = true
+
+		qs := url.Values{}
+
+		fmt.Println(qs)
+		fmt.Println(url.Values{})
+
+
+		for param, vv := range u.Query() {
+			qs.Set(param, vv[0])
+		}
+
+		for param, vv := range u.Query() {
+			if appendMode {
+				qs.Set(param, vv[0]+flag.Arg(0))
+			} else {
+				qs.Set(param, flag.Arg(0))
+			}
+			u.RawQuery = qs.Encode()
+			fmt.Printf("%s\n", u)
+			qs.Set(param, vv[0])
+
+
+		}
+
+
+	}
+
+}
